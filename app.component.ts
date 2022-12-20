799062185a54cc71e80920548899436ae5b9213e
smita <120364955+patilsmita1@users.noreply.github.com>
2022-12-20 10:09:10 +0530

    Assignment no 4

---
+++ app.component.ts
@@ -0,0 +1,18 @@
+import { Component,OnInit, VERSION} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  title = 'SmitaAngularapplication';
+
+  
+  constructor(){
+
+  }
+  ngOnInit():void{
+    
+  }
+}
